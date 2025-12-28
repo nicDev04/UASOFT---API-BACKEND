6dc05b02c5580ec8d26f2d477642bf4d7b52b5ca
nicDev04 <contato.nicolas04@gmail.com>
2025-12-28 18:07:38 -0300

    corrigido login

--- src/main/java/com/nicolas/uasoft/controller/LoginController.java
+++ src/main/java/com/nicolas/uasoft/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,6 +17,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/")
 public class LoginController {
 
     private final LoginService loginService;
@@ -32,8 +34,11 @@ public class LoginController {
         Map<String, Object> loginResponse = new HashMap<>();
 
         if (usuario == null) {
-            loginResponse.put("usuario", usuario);
+            loginResponse.put("mensagem", "Usuário ou senha inválidos");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(loginResponse);
+        }  else {
+            loginResponse.put("mensagem", "Login efetuado com sucesso");
+            loginResponse.put("usuario", usuario);
         }
 
         String token = UUID.randomUUID().toString();
